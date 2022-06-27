d2f7aea4cf0de79d1fbcf27141aae37169d6c3c5
Abhishek Bisht <99400134+abhibisht01@users.noreply.github.com>
2022-06-27 20:34:00 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@
 import firebase from 'firebase/compat/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5Rq5MDQ8I2JsbsnPufP2VymuPy6xlpc0",
-  authDomain: "cryptopunk-cl.firebaseapp.com",
-  projectId: "cryptopunk-cl",
-  storageBucket: "cryptopunk-cl.appspot.com",
-  messagingSenderId: "5278550979",
-  appId: "1:5278550979:web:e43ae580f29d8fa68b8b76",
-  measurementId: "G-VD55C6EBC5"
+MAke Yours..
 };
 
 // Initialize Firebase
