ff4adf09adcfe260abdcd05df9729ea53dfcf15a
Abhishek Bisht <bisht.abhishek77@gmail.com>
2022-06-27 20:14:56 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       const fetchData = async () => {
         const response = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x387d51b9f82738eAA8cEcfBc414f1bcC6E1fb8F8&order_direction=asc');
 
-        // copy the adress of the third web collection aka the project 
+        // copy the adress of the third web collection aka the  custom project on third web  not the wallet
 
         console.log(response.data.assets)
         setPunkListData(response.data.assets)
