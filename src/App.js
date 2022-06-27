bbfc0a769c924afee62cf977ab8a7f2e76fda34b
Abhishek Bisht <bisht.abhishek77@gmail.com>
2022-06-27 20:07:44 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+import PunkList from './components/PunkList';
+import Main from './components/Main';
+
 
 function App() {
+
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0)
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x387d51b9f82738eAA8cEcfBc414f1bcC6E1fb8F8&order_direction=asc');
+        // copy the adress of the third web collection aka the project 
+
+        console.log(response.data.assets)
+        setPunkListData(response.data.assets)
+
+      }
+      fetchData();
+    }
+    catch (error) {
+      throw new Error(error)
+
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      {
+        punkListData.length > 0 && (
+          <>
+            <Main punkListData={punkListData} selectedPunk={selectedPunk} />
+            <PunkList punkListData={punkListData} setSelectedPunk={setSelectedPunk} />
+          </>
+
+        )
+      }
+
     </div>
   );
 }
